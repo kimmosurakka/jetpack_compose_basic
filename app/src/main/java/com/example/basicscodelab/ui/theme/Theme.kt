3dc950e55ea3872b873804ec256a2fb04e9385f9
Kimmo Surakka <kimmo.surakka@gmail.com>
2022-04-13 09:11:01 +0300

    Styling and theming

--- app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
@@ -5,25 +5,27 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 
         /* Other default colors to override
+    primaryVariant = Purple700,
     background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
     onBackground = Color.Black,
-    onSurface = Color.Black,
+    secondary = Teal200,
+    onSecondary = Color.Black,
     */
 )
 
